a2fd02a2f8c0407c00d03b13acfee3da5a5b0b3e
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-09-28 14:04:37 +0700

    deplooy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3001
+const port = process.env.PORT || 3001
 
 const session = require('express-session')
 app.use(session({
